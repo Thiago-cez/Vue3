ea30efe56fc3f4c3f31bd2d37d71e806a58a458c
Thiago-Cez <thiago.silva.ts804@gmail.com>
2024-02-21 23:40:22 +0000

    feat: finish class 8

--- aprendendo-vue/src/App.vue
+++ aprendendo-vue/src/App.vue
@@ -1,15 +1,61 @@
 <template>
   <HelloWorld msg="Class4 App"/>
-  <h1>LISTA DE USUARIOS: </h1>
-  <div
-      v-for="usuario in users"   
-      v-bind:key="usuario.id"
-   >
-    <h1>Nome: {{ usuario.name }}</h1>
-    <h1>email: {{ usuario.email }}</h1>
-    <h1>Telefone: {{ usuario.phone }}</h1>
-    <h1>Compania: {{ usuario.company.name }}</h1>
-  </div>
+  
+    <div>
+    <h1> -- PRIMEIRO EXEMPLO -- </h1>
+    <input id="inputField" v-model="teste1" placeholder="Digite algo aqui e veja a magica acontecer"><br>
+    {{ teste1 }}
+
+    </div><br>
+
+    <div>
+      <h1> -- SEGUNDO EXEMPLO -- </h1>
+      <h4> -- LISTA DE ESCOLHA -- </h4>
+
+      <label> Escolha um ESPORTE </label><br>
+      <select v-model="teste2" value="">
+        <option value="">Escolha</option>
+        <option value="Futebol">Futebol</option>
+        <option value="Handball">Handball</option>
+        <option value="Volei">Volei</option>
+        <option value="Natação">Natação</option>
+        <option value="Corrida">Corrida</option>
+        <option value="Judô">Judô</option>
+        <option value="Tiro Livre">Tiro Livre</option>
+        <option value="Baseball">Baseball</option>
+        <option value="American Football">American Football</option>
+      </select>
+
+      <p> Voce escolheu: {{ teste2 }}</p>
+    </div>
+
+    <div>
+      <h1> -- TERCEIRO EXEMPLO -- </h1>
+      <h4> -- ESCOLHA VIA RADIO BUTTON -- </h4>
+
+      <label> Gostas desse tipo de Exemplo </label><br>
+       <input v-model="teste3" type="radio" value="SIM">SIM 
+       <input v-model="teste3" type="radio" value="NÃO" >NÃO 
+
+      <p> Voce escolheu: {{ teste3 }}</p>
+    </div>
+
+    <div>
+      <h1> -- QUARTO EXEMPLO -- </h1>
+      <h4> -- ESCOLHA VIA CHECK-BOX -- </h4>
+
+      <label> FRUTARIA:  </label><br><br>
+       <input v-model="frutaria" type="checkbox" value="BANANA"> Banana 
+       <input v-model="frutaria" type="checkbox" value="MACA" > Maça 
+       <input v-model="frutaria" type="checkbox" value="PERA" > Pera 
+       <input v-model="frutaria" type="checkbox" value="KIWI" > Kiwi 
+       <input v-model="frutaria" type="checkbox" value="UVA" > Uva 
+       <input v-model="frutaria" type="checkbox" value="MELAO" > Melão  
+       <input v-model="frutaria" type="checkbox" value="MANGA" > Manga
+       <input v-model="frutaria" type="checkbox" value="MELANCIA" > Melancia 
+
+      <p> Voce escolheu: {{ frutaria }}</p>
+    </div>
   
 </template>
 
@@ -24,146 +70,10 @@ export default {
 
   data() {
        return {
-        users: [
-                    {
-                      "id": 1,
-                      "name": "Leanne Graham",
-                      "username": "Bret",
-                      "email": "Sincere@april.biz",
-                      "address": {
-                        "street": "Kulas Light",
-                        "suite": "Apt. 556",
-                        "city": "Gwenborough",
-                        "zipcode": "92998-3874",
-                        "geo": {
-                          "lat": "-37.3159",
-                          "lng": "81.1496"
-                        }
-                      },
-                      "phone": "1-770-736-8031 x56442",
-                      "website": "hildegard.org",
-                      "company": {
-                        "name": "Romaguera-Crona",
-                        "catchPhrase": "Multi-layered client-server neural-net",
-                        "bs": "harness real-time e-markets"
-                      }
-                    },
-                    {
-                      "id": 2,
-                      "name": "Ervin Howell",
-                      "username": "Antonette",
-                      "email": "Shanna@melissa.tv",
-                      "address": {
-                        "street": "Victor Plains",
-                        "suite": "Suite 879",
-                        "city": "Wisokyburgh",
-                        "zipcode": "90566-7771",
-                        "geo": {
-                          "lat": "-43.9509",
-                          "lng": "-34.4618"
-                        }
-                      },
-                      "phone": "010-692-6593 x09125",
-                      "website": "anastasia.net",
-                      "company": {
-                        "name": "Deckow-Crist",
-                        "catchPhrase": "Proactive didactic contingency",
-                        "bs": "synergize scalable supply-chains"
-                      }
-                    },
-                    {
-                      "id": 3,
-                      "name": "Clementine Bauch",
-                      "username": "Samantha",
-                      "email": "Nathan@yesenia.net",
-                      "address": {
-                        "street": "Douglas Extension",
-                        "suite": "Suite 847",
-                        "city": "McKenziehaven",
-                        "zipcode": "59590-4157",
-                        "geo": {
-                          "lat": "-68.6102",
-                          "lng": "-47.0653"
-                        }
-                      },
-                      "phone": "1-463-123-4447",
-                      "website": "ramiro.info",
-                      "company": {
-                        "name": "Romaguera-Jacobson",
-                        "catchPhrase": "Face to face bifurcated interface",
-                        "bs": "e-enable strategic applications"
-                      }
-                    },
-                    {
-                      "id": 4,
-                      "name": "Patricia Lebsack",
-                      "username": "Karianne",
-                      "email": "Julianne.OConner@kory.org",
-                      "address": {
-                        "street": "Hoeger Mall",
-                        "suite": "Apt. 692",
-                        "city": "South Elvis",
-                        "zipcode": "53919-4257",
-                        "geo": {
-                          "lat": "29.4572",
-                          "lng": "-164.2990"
-                        }
-                      },
-                      "phone": "493-170-9623 x156",
-                      "website": "kale.biz",
-                      "company": {
-                        "name": "Robel-Corkery",
-                        "catchPhrase": "Multi-tiered zero tolerance productivity",
-                        "bs": "transition cutting-edge web services"
-                      }
-                    },
-                    {
-                      "id": 5,
-                      "name": "Chelsey Dietrich",
-                      "username": "Kamren",
-                      "email": "Lucio_Hettinger@annie.ca",
-                      "address": {
-                        "street": "Skiles Walks",
-                        "suite": "Suite 351",
-                        "city": "Roscoeview",
-                        "zipcode": "33263",
-                        "geo": {
-                          "lat": "-31.8129",
-                          "lng": "62.5342"
-                        }
-                      },
-                      "phone": "(254)954-1289",
-                      "website": "demarco.info",
-                      "company": {
-                        "name": "Keebler LLC",
-                        "catchPhrase": "User-centric fault-tolerant solution",
-                        "bs": "revolutionize end-to-end systems"
-                      }
-                    },
-                    {
-                      "id": 6,
-                      "name": "Mrs. Dennis Schulist",
-                      "username": "Leopoldo_Corkery",
-                      "email": "Karley_Dach@jasper.info",
-                      "address": {
-                        "street": "Norberto Crossing",
-                        "suite": "Apt. 950",
-                        "city": "South Christy",
-                        "zipcode": "23505-1337",
-                        "geo": {
-                          "lat": "-71.4197",
-                          "lng": "71.7478"
-                        }
-                      },
-                      "phone": "1-477-935-8478 x6430",
-                      "website": "ola.org",
-                      "company": {
-                        "name": "Considine-Lockman",
-                        "catchPhrase": "Synchronised bottom-line interface",
-                        "bs": "e-enable innovative applications"
-                      }
-                    }
-                  ]
+         teste1: "Thiago Cezario",
+         teste2: "",
+         teste3: "",
+         frutaria: []
        }
   }
 
@@ -171,6 +81,14 @@ export default {
 </script>
 
 <style>
+
+#inputField {
+  height: 30px;
+  width: 300px;
+  text-align: center;
+  font-size: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
